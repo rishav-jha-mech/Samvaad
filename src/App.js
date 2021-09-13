047a8b25404f661fe57d0f1990b9cb2164f573f9
rishav-jha-mech <rishav.rj74@gmail.com>
2021-09-13 16:46:34 +0530

    Changes in Navbar & Homepage section1 made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import NewsPage from './Components/Pages/NewsPage'
+import Navbar from './Components/Navbar'
+import Homepage from './Components/Pages/Homepage';
+import './App.css'
 
 function App() {
   return (
-    <NewsPage />
+    <>
+    <Navbar />
+    <Homepage />
+    </>
   );
 }
 
--- src/Components/Pages/Homepage.js
+++ src/Components/Pages/Homepage.js
@@ -1,39 +1,25 @@
-import React from 'react'
+import './css/homepage.css'
+import landingImage from './images/landing.png'
 
 function Homepage() {
     return (<>
-        <section>
-            <h1>Nice Curves</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab laborum expedita cumque modi dignissimos minus molestias provident, saepe iusto sapiente. Nostrum ullam quibusdam laborum ab deserunt officiis ratione sapiente. Aut.</p>
+        <div className="HomeParent">
+        <section className="page1">
+            <div className="text">
+                <p> Welcome to SamVaad  </p>
+                <h1>  World's most trusted News website.</h1>
+                <br />
+                <h5> Get the latest news, top headlines, opinions, analysis and much more from India and World including current news headlines on elections, politics, economy, weather, business.</h5>
+                <div className="button-container">
+                    <a href="#">Read News</a>
+                    <a href="#">Download App</a>
+                </div>
+            </div>
+            <div className="image">
+                <img src={landingImage} />
+            </div>
         </section>
-        <section>
-            <h1>Nice Curves</h1>
-            <p>Alias voluptatum pariatur eveniet mollitia. Voluptates aliquam dignissimos, aperiam pariatur ullam nisi fugit excepturi provident fugiat dolor autem cumque accusantium tenetur non itaque! Consequuntur sed perspiciatis aut et vitae fuga.</p>
-        </section>
-        <section>
-            <h1>Nice Curves</h1>
-            <p>Quod sequi blanditiis ducimus omnis repellendus ab nulla, porro corrupti ipsam dolores? Dolores atque eaque, minus laboriosam molestiae excepturi, deleniti libero hic tenetur voluptatem quisquam assumenda odit, natus fugit perspiciatis.</p>
-        </section>
-        <section>
-            <h1>Nice Curves</h1>
-            <p>Nulla, sint ea? Quod minus maiores fugiat voluptatibus molestiae sit, blanditiis dicta cupiditate totam consequuntur saepe aliquam placeat eius sequi qui excepturi mollitia, nemo obcaecati inventore tempora provident, laborum aspernatur?</p>
-        </section>
-        <section>
-            <h1>Nice Curves</h1>
-            <p>Ducimus nostrum voluptatem tempore sed consequuntur inventore quis, amet quos cupiditate vitae obcaecati voluptatibus molestias dolorum sunt harum numquam ut exercitationem eum iure et earum accusantium placeat? Quo, corrupti aut?</p>
-        </section>
-        <section>
-            <h1>Nice Curves</h1>
-            <p>Minima, explicabo blanditiis quam nulla, nemo facere incidunt mollitia expedita distinctio doloremque voluptates similique consequatur fugit. Ullam magnam sunt sit ex aut, quasi in similique amet illo hic rem corporis?</p>
-        </section>
-        <section>
-            <h1>Nice Curves</h1>
-            <p>Adipisci itaque sunt, magni numquam ullam debitis assumenda repudiandae quia neque maxime! Deserunt voluptates esse mollitia quasi ea? Quasi consectetur rerum quas mollitia illo impedit necessitatibus voluptate facere recusandae eum!</p>
-        </section>
-        <section>
-            <h1>Nice Curves</h1>
-            <p>Suscipit dignissimos nam beatae. Sint atque illo, ipsam suscipit quia mollitia commodi. Neque dolorem soluta accusamus minima ducimus fugit consequuntur architecto veritatis hic non maxime incidunt praesentium, magni eos aliquam.</p>
-        </section>
+        </div>
     </>)
 }
 
